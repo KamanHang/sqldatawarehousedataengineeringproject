1e1bb7314913a416244216a68d5df13be1f58270
KamanHang <kamanhangshrong@gmail.com>
2025-01-23 10:40:13 +0545

    Update on Bronze Data Load File

--- ProjectScripts/BronzeScripts/bronze_data_load.sql
+++ ProjectScripts/BronzeScripts/bronze_data_load.sql
@@ -1,15 +1,16 @@
--- -- In postgres sql run the copy command for bulk insert in pgsql cli
+-- Purpose: 
+	-- The purpose of this SQL Script is to bulk insert data from csv files stored locally.
 
 TRUNCATE TABLE bronze.crm_cust_info;
 \COPY bronze.crm_cust_info FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/cust_info.csv' DELIMITER ',' CSV HEADER;
-SELECT count(*) FROM bronze.crm_cust_info;
+-- SELECT count(*) FROM bronze.crm_cust_info;
 
 TRUNCATE TABLE bronze.crm_prd_info;
 \COPY bronze.crm_prd_info FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' DELIMITER ',' CSV HEADER;
-SELECT count(*) FROM bronze.crm_prd_info;
+-- SELECT count(*) FROM bronze.crm_prd_info;
 
 
 TRUNCATE TABLE bronze.crm_sales_details;
 \COPY bronze.crm_sales_details FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' DELIMITER ',' CSV HEADER;
-SELECT count(*) FROM bronze.crm_sales_details;
+-- SELECT count(*) FROM bronze.crm_sales_details;
 
