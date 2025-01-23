61082dd3ddb4fba7a66ea7a20f3ad4c320605469
KamanHang <kamanhangshrong@gmail.com>
2025-01-23 10:36:56 +0545

    Update on CRM_TABLE_DDL File

---
+++ ProjectScripts/BronzeScripts/bronze_data_load.sql
@@ -0,0 +1,15 @@
+-- -- In postgres sql run the copy command for bulk insert in pgsql cli
+
+TRUNCATE TABLE bronze.crm_cust_info;
+\COPY bronze.crm_cust_info FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/cust_info.csv' DELIMITER ',' CSV HEADER;
+SELECT count(*) FROM bronze.crm_cust_info;
+
+TRUNCATE TABLE bronze.crm_prd_info;
+\COPY bronze.crm_prd_info FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' DELIMITER ',' CSV HEADER;
+SELECT count(*) FROM bronze.crm_prd_info;
+
+
+TRUNCATE TABLE bronze.crm_sales_details;
+\COPY bronze.crm_sales_details FROM '/Users/mecco/Downloads/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' DELIMITER ',' CSV HEADER;
+SELECT count(*) FROM bronze.crm_sales_details;
+
