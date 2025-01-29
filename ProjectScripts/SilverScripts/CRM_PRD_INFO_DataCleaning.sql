65a5a6479a2434569af607da87b6b4bb2598078b
KamanHang <kamanhangshrong@gmail.com>
2025-01-29 23:48:30 +0545

    Completed Data Cleaning Script for CRM CUST & PRD INFO

---
+++ ProjectScripts/SilverScripts/CRM_PRD_INFO_DataCleaning.sql
@@ -0,0 +1,17 @@
+-- Below Scripts is for cleaning the data present in crm_prd_info table.
+SELECT 
+prd_id,  
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id, -- Will be used For joining erp_px_cat_g1v2 table
+SUBSTRING(prd_key,7,LENGTH(prd_key)) AS prd_key, -- Will be used for joining sales_details table
+prd_nm,
+COALESCE(prd_cost, 0), -- Replacing Null Values with Zero
+CASE UPPER(TRIM(prd_line)) 
+	WHEN 'M' THEN 'Mountain'
+	WHEN 'R' THEN 'Road'
+	WHEN 'S' THEN 'Other Sales'
+	WHEN 'T' THEN 'Touring'
+	ELSE 'n/a'
+END prd_line, -- Got the Values from Source System Expert
+prd_start_dt,
+LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt -- Fixed the date order issue
+FROM bronze.crm_prd_info;
--- ProjectScripts/SilverScripts/CleaningInfo.sql
+++ ProjectScripts/SilverScripts/CleaningInfo.sql
@@ -21,6 +21,17 @@ FROM bronze.crm_cust_info;
 -- DATA Standardization and Consistency
 SELECT DISTINCT cst_gndr FROM bronze.crm_cust_info;
 -- For data consistency I have replaced the gender M as Male and F as Female using CASE WHEN statement
+SELECT DISTINCT prd_line FROM bronze.crm_prd_info;
 
+-- Checking if End Date is Earlier than the Start Date
+SELECT * FROM bronze.crm_prd_info WHERE prd_end_dt < prd_start_dt;
 
-
+SELECT 
+prd_id,
+prd_key, 
+prd_nm, 
+prd_start_dt, 
+LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)AS prd_start_Date_test,
+prd_end_dt 
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509');
